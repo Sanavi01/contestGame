99a528d86b7748b2b72e33dcdc16271859bf2f82
Santiago Avila <sanavila2002@gmail.com>
2022-03-23 11:38:23 -0500

    Se crean los archivos index, script y styles para el respectivo codigo Script.js: Se crea la clase Questions con la cual se crearan todas las preguntas para el concurso Se instancia la el primer objeto de clase Questions con tal de verificar el funcionamiento

---
+++ script.js
@@ -0,0 +1,22 @@
+class Questions {
+    constructor (categoria, pregunta, nivel, correcta, incorrecta1, incorrecta2, incorrecta3 ){
+        this.categoria = categoria
+        this.pregunta = pregunta
+        this.nivel = nivel
+        this.correcta = correcta
+        this.incorrecta1 = incorrecta1
+        this.incorrecta2 = incorrecta2
+        this.incorrecta3 = incorrecta3
+    }
+    
+}
+
+const matPreguntaUno = new Questions ('Matematicas',
+                                    'Cuál es el número anterior a 1000',
+                                     1,
+                                     999,
+                                     990,
+                                     1001,
+                                     900)
+
+console.log(matPreguntaUno)                                  
